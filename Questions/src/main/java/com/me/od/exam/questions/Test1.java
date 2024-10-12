327fb0c969954fb44a9029cda9d55ec5f1c47d13
liyuehang <12452558+real_osmond@user.noreply.gitee.com>
2024-10-13 00:23:48 +0800

    init project

---
+++ Questions/src/main/java/com/me/od/exam/questions/Test1.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023-2024, yuehangdada All rights reserved.
+ *
+ */
+package com.me.od.exam.questions;
+
+/**
+ * @author yuehangdada
+ * @version 1.0
+ * @description
+ * @date 2024-10-13 00:19
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("1 = " + 1);
+    }
+}
